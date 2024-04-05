e4e9b46dafc05ebc81499d95971b0943f11b95b5
otakakot <kotaro.otaka@gmail.com>
2024-04-06 01:44:59 +0900

    first commit

--- nilzero.go
+++ nilzero.go
@@ -1,7 +1,5 @@
 package nilex
 
-import "reflect"
-
 // NilZero returns the zero value of the type of the pointer v if v is nil, otherwise it returns the value of v.
 func NilZero[T any](v *T) T {
 	if v == nil {
@@ -11,17 +9,4 @@ func NilZero[T any](v *T) T {
 	}
 
 	return *v
-}
-
-func IsZero[T any](v *T) bool {
-	t := reflect.TypeOf(v)
-
-	switch t.Kind() {
-	case reflect.Ptr, reflect.Interface:
-		return v == nil
-	case reflect.Slice, reflect.Map, reflect.Array:
-		return reflect.ValueOf(v).Len() == 0
-	default:
-		return reflect.DeepEqual(v, reflect.Zero(t).Interface())
-	}
 }
